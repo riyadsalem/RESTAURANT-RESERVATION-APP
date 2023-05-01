dcc3419348c9d8a1c4a1878c96321cbc3c465d1c
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-05-01 07:11:13 -0700

    Nested Components & Updating the Head Meta For Each Page Release #1

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,7 +1,5 @@
-import { NavBar } from "../../components";
 import {
   Description,
-  Header,
   Images,
   Rating,
   ReservationCard,
@@ -12,24 +10,18 @@ import {
 
 export default function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            <RestaurantNavBar />
-            <Title />
-            <Rating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
-        </div>
-      </main>
-    </main>
+    <>
+      <div className="bg-white w-[70%] rounded p-3 mb-4 shadow">
+        <RestaurantNavBar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
+      </div>
+    </>
   );
 }
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,19 +1,16 @@
-import { Header, NavBar } from "../components";
+import { Header } from "../components";
 import { RestaurantCard, SearchSideBar } from "./components";
 
 export default function Search() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <Header search="search" />
-        <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-          <SearchSideBar />
-          <div className="w-5/6">
-            <RestaurantCard />
-          </div>
+    <>
+      <Header search="search" />
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+        <SearchSideBar />
+        <div className="w-5/6">
+          <RestaurantCard />
         </div>
-      </main>
-    </main>
+      </div>
+    </>
   );
 }

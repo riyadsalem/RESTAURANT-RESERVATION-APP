ffb25853efb8dd81f6d20c1da85e115a75d3e0f6
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-05-03 07:36:04 -0700

    Fetching a Restaurant By Slug Release #3

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { type } from "os";
 import {
   Description,
   Images,
@@ -8,7 +9,48 @@ import {
   Title,
 } from "./components";
 
-export default function RestaurantDetails() {
+import { PrismaClient } from "@prisma/client";
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant;
+};
+
+export default async function RestaurantDetails({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  //   props: { params: { slug: 'coconut-lagoon-ottawa' }, searchParams: {} }
+
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 mb-4 shadow">

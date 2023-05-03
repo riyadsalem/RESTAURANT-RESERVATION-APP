3982bfbd31c4b6866017d4550fbeca9cda7dc31d
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-05-03 01:39:48 -0700

    Seeding Our Database Release #3

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body>
-        <main className="bg-gray-100 min-h-screen w-screen">
+        <main className="bg-gray-100 min-h-screen">
           <main className="max-w-screen-2xl m-auto bg-white">
             <NavBar />
             {children}

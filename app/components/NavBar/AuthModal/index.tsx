475ff7ed5c65812bcb9b73cb814712baf476130c
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-05-08 10:05:57 +0300

    Adding the Modal Content Release #7

--- app/components/NavBar/AuthModal/index.tsx
+++ app/components/NavBar/AuthModal/index.tsx
@@ -2,8 +2,8 @@
 
 import React, { useState } from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import AuthModalInputs from "./AuthModalInputs";
 
 const style = {
   position: "absolute" as "absolute",
@@ -12,7 +12,6 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
@@ -45,12 +44,25 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <div className="p-2 h-[600px]">
+            <div className="uppercase font-bold text-center pb-2 border-b mb-2">
+              <p className="text-sm">
+                {renderContent("Sign In", "Create Account")}
+              </p>
+            </div>
+            <div className="m-auto">
+              <h2 className="text-2xl font-light text-center">
+                {renderContent(
+                  "Log Into Your Account",
+                  "Create Your OpenTable Account"
+                )}
+              </h2>
+              <AuthModalInputs />
+              <button className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400">
+                {renderContent("Sign In", "Create Account")}
+              </button>
+            </div>
+          </div>
         </Box>
       </Modal>
     </div>

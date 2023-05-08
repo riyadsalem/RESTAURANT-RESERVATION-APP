3aaaa08d4890f77d5c13e349a12dbfe63266b9b2
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-05-08 19:22:20 +0300

    Implementing the SignIn Endpoint Release #7

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -6,9 +6,18 @@ import * as jose from "jose";
 
 const prisma = new PrismaClient();
 
+interface Response {
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  phone?: string;
+  city?: string;
+  errorMessage?: string;
+}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<Response>
 ) {
   if (req.method === "POST") {
     const { firstName, lastName, email, phone, city, password } = req.body;
@@ -82,14 +91,20 @@ export default async function handler(
     });
 
     const alg = "HS256";
-
     const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
     const token = await new jose.SignJWT({ email: user.email })
       .setProtectedHeader({ alg })
       .setExpirationTime("24h")
       .sign(secret);
 
-    return res.status(200).json({ token });
-  } // End if (metho === "POST")
+    return res.status(200).json({
+      firstName: user.first_name,
+      lastName: user.last_name,
+      email: user.email,
+      phone: user.phone,
+      city: user.city,
+    });
+  }
+
+  return res.status(404).json({ errorMessage: "Unknown endpoint" });
 }

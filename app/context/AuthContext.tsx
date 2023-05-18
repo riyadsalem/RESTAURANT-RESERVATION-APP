5c9b0cc644fee0adb97e43f7ffede4bc637b4bb4
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-05-18 20:32:12 +0300

    Persisting an Auth State Release #9

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import axios from "axios";
-import { getCookie } from "cookies-next";
+import { CookieValueTypes, getCookie } from "cookies-next";
 import { useState, createContext, useEffect } from "react";
 
 interface User {
-  id: number;
+  id?: number;
   firstName: string;
   lastName: string;
-  email: string;
+  email?: string;
   city: string;
   phone: string;
 }
@@ -40,6 +40,49 @@ export default function AuthContext({
     data: null,
     error: null,
   });
+
+  const fetchUser = async (): Promise<void> => {
+    setAuthState({
+      data: null,
+      error: null,
+      loading: true,
+    });
+    try {
+      const jwt: CookieValueTypes = getCookie("jwt");
+
+      if (!jwt) {
+        return setAuthState({
+          data: null,
+          error: null,
+          loading: false,
+        });
+      }
+
+      const response = await axios.get("http://localhost:3000/api/auth/me", {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+
+      axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+
+      setAuthState({
+        data: response.data,
+        error: null,
+        loading: false,
+      });
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        error: error.response.data.errorMessage,
+        loading: false,
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
   return (
     <AuthenticationContext.Provider

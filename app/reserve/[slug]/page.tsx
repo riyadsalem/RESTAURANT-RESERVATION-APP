55576945c3f7cbc6b19658d8828c094da3761311
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-05-25 18:17:18 +0300

    Dynamically Rendering Date for the Reserve Header & Extracting the Query Params Release #13

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,10 +1,36 @@
+import { PrismaClient } from "@prisma/client";
 import { Form, Header } from "./components";
+import { notFound } from "next/navigation";
 
-export default function Reserve() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+  if (!restaurant) notFound();
+  return restaurant;
+};
+
+export default async function Reserve({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { date: string; partySize: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <Header />
+        <Header
+          image={restaurant.main_image}
+          name={restaurant.name}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
         <Form />
       </div>
     </div>

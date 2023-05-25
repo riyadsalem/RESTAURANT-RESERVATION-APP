75038294b25b10a443d57f2269d2a06f644d85fa
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-05-25 13:32:29 +0300

    Determining the Available Tables & Count the Tables Based on Seats & Determine the Tables to Book Release #13

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -55,8 +55,74 @@ export default async function handler(
     }
 
     // Determine all available tables based on that datetime and partySize
+    const searchTimesWithTables = await findAvailabileTables({
+      day,
+      time,
+      res,
+      restaurant,
+    });
 
-    res.status(200).json({ Message: "Not Found Error" });
+    const searchTimeWithTables = searchTimesWithTables?.find((t) => {
+      return t.date.toISOString() === new Date(`${day}T${time}`).toISOString();
+    });
+
+    if (!searchTimeWithTables) {
+      return res.status(400).json({
+        errorMessage: "No availablity, cannot book",
+      });
+    }
+
+    // Count The Tables Based on Seats
+    const tablesCount: {
+      2: number[];
+      4: number[];
+    } = {
+      2: [],
+      4: [],
+    };
+
+    searchTimeWithTables.tables?.forEach((table) => {
+      if (table.seats === 2) {
+        tablesCount[2].push(table.id);
+      } else {
+        tablesCount[4].push(table.id);
+      }
+    });
+
+    // Determine the tables to book which leads to the least number of seats used up
+    const tablesToBooks: number[] = [];
+    let seatsRemaining = parseInt(partySize);
+
+    while (seatsRemaining > 0) {
+      if (seatsRemaining >= 3) {
+        if (tablesCount[4].length) {
+          tablesToBooks.push(tablesCount[4][0]);
+          tablesCount[4].shift();
+          seatsRemaining = seatsRemaining - 4;
+        } else {
+          tablesToBooks.push(tablesCount[2][0]);
+          tablesCount[2].shift();
+          seatsRemaining = seatsRemaining - 2;
+        }
+      } else {
+        if (tablesCount[2].length) {
+          tablesToBooks.push(tablesCount[2][0]);
+          tablesCount[2].shift();
+          seatsRemaining = seatsRemaining - 2;
+        } else {
+          tablesToBooks.push(tablesCount[4][0]);
+          tablesCount[4].shift();
+          seatsRemaining = seatsRemaining - 4;
+        }
+      }
+    }
+
+    res.status(200).json({
+      Message: "Not Found Error",
+      searchTimeWithTables,
+      tablesCount,
+      tablesToBooks,
+    });
   }
 }
 
